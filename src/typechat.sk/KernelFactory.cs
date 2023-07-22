2dfa09c91aaff7ab0b5e2056f89b370dce8be74f
Umesh Madan <umeshma@microsoft.com>
2023-07-21 17:25:14 -0700

    Hooked up auto-schema generation

--- src/typechat.sk/KernelFactory.cs
+++ src/typechat.sk/KernelFactory.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using Microsoft.TypeChat.SemanticKernel.Typescript;
+
 namespace Microsoft.TypeChat.SemanticKernel;
 
 public static class KernelFactory
@@ -14,8 +16,28 @@ public static class KernelFactory
         return kernel.JsonTranslator<T>(schema, model);
     }
 
+    public static TypeChatJsonTranslator<T> JsonTranslator<T>(ModelInfo model, OpenAIConfig config)
+    {
+        // Create kernel
+        KernelBuilder kb = new KernelBuilder();
+        kb.WithChatModel(model.Name, config);
+        IKernel kernel = kb.Build();
+        // And Json translator
+        return kernel.JsonTranslator<T>(model);
+    }
+
     public static TypeChatJsonTranslator<T> JsonTranslator<T>(this IKernel kernel, Schema schema, ModelInfo model)
     {
+        TypeChatJsonTranslator<T> typechat = new TypeChatJsonTranslator<T>(
+            kernel.CompletionService(model),
+            new JsonSerializerTypeValidator<T>(schema)
+        );
+        return typechat;
+    }
+
+    public static TypeChatJsonTranslator<T> JsonTranslator<T>(this IKernel kernel, ModelInfo model)
+    {
+        TypeSchema schema = TypescriptExporter.GenerateSchema(typeof(T));
         TypeChatJsonTranslator<T> typechat = new TypeChatJsonTranslator<T>(
             kernel.CompletionService(model),
             new JsonSerializerTypeValidator<T>(schema)
