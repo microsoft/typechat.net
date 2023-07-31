2587c84b1ec7162f4789aa13f9b6acb5da30651f
Umesh Madan <umeshma@microsoft.com>
2023-07-31 13:53:22 -0700

    Program expt

--- src/typechat.program/Program.cs
+++ src/typechat.program/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 namespace Microsoft.TypeChat;
-
+/*
 [Comment("A program consists of a sequence of function calls that are evaluated in order")]
 public class Program
 {
@@ -14,11 +14,10 @@ public class Program
 [JsonDerivedType(typeof(Call), typeDiscriminator: nameof(Call))]
 [JsonDerivedType(typeof(ResultRef), typeDiscriminator: nameof(ResultRef))]
 [JsonDerivedType(typeof(Value), typeDiscriminator: nameof(Value))]
-public abstract class Expr { }
+public interface IExpr { }
 
-[Comment("Value are ONE of the fo")]
 [Comment("type Json Value = string | number | boolean | null | Expr[]")]
-public class Value : Expr
+public class Value : IExpr
 {
     public enum Type
     {
@@ -32,24 +31,36 @@ public class Value : Expr
     public string? String { get; set; }
     public double? Number { get; set; }
     public bool? Bool { get; set; }
-    public Expr[]? Array { get; set; }
+    public IExpr[]? Array { get; set; }
 }
 
 [Comment("A function call specifies a function name and a list of argument expressions.")]
 [Comment("Arguments may contain nested function calls and result references.")]
-public class Call : Expr
+public class Call : IExpr
 {
     // Name of the function
     [JsonPropertyName("func")]
     public string Func { get; set; }
     // Arguments for the function, if any
     [JsonPropertyName("args")]
-    public Expr[]? Args { get; set; }
+    public IExpr[]? Args { get; set; }
 };
 
-public class ResultRef : Expr
+public class ResultRef : IExpr
 {
     // Index of the previous expression in the "@steps" array
     [JsonPropertyName("ref")]
     public int Ref { get; set; }
+}
+*/
+
+public class Program
+{
+    [JsonPropertyName("steps")]
+    public Call[] Steps { get; set; }
+}
+
+public class Call
+{
+
 }
