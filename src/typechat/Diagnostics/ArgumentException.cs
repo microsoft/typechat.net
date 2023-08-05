0aa246e04e7e1d6c2630de81fa00bcc5391bff63
Shawn Callegari <36091529+shawncal@users.noreply.github.com>
2023-08-04 20:16:49 -0700

    Converting typechat.csproj to netstandard2.0. Including missing types as internal.

---
+++ src/typechat/Diagnostics/ArgumentException.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Microsoft.TypeChat;
+
+internal static class ArgumentException
+{
+    /// <summary>Throws an exception if <paramref name="argument"/> is null or empty.</summary>
+    /// <param name="argument">The string argument to validate as non-null and non-empty.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="argument"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="argument"/> is empty.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        if (string.IsNullOrEmpty(argument))
+        {
+            ThrowNullOrEmptyException(argument, paramName);
+        }
+    }
+
+    /// <summary>Throws an exception if <paramref name="argument"/> is null, empty, or consists only of white-space characters.</summary>
+    /// <param name="argument">The string argument to validate.</param>
+    /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="argument"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="argument"/> is empty or consists only of white-space characters.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ThrowIfNullOrWhiteSpace([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        if (string.IsNullOrWhiteSpace(argument))
+        {
+            ThrowNullOrWhiteSpaceException(argument, paramName);
+        }
+    }
+
+    [DoesNotReturn]
+    private static void ThrowNullOrEmptyException(string? argument, string? paramName)
+    {
+        ArgumentNullException.ThrowIfNull(argument, paramName);
+        throw new System.ArgumentException("The value cannot be an empty string.", paramName);
+    }
+
+    [DoesNotReturn]
+    private static void ThrowNullOrWhiteSpaceException(string? argument, string? paramName)
+    {
+        ArgumentNullException.ThrowIfNull(argument, paramName);
+        throw new System.ArgumentException("The value cannot be an empty string or composed entirely of whitespace.", paramName);
+    }
+}
