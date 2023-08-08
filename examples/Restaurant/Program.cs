d67abd98ee2589d2e50ec9eff4f16f6d347af407
Umesh Madan <umeshma@microsoft.com>
2023-08-08 10:23:46 -0700

    Restaurant Example. Cleanup of Coffeeshop

---
+++ examples/Restaurant/Program.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.TypeChat;
+using Microsoft.TypeChat.Schema;
+using Microsoft.TypeChat.SemanticKernel;
+
+namespace Restaurant;
+
+public class Restaurant : ConsoleApp
+{
+    JsonTranslator<Order> _translator;
+
+    Restaurant()
+    {
+        _translator = KernelFactory.JsonTranslator<Order>(Config.LoadOpenAI());
+        _translator.MaxRepairAttempts = 3;
+        // Uncomment to see ALL raw messages to and from the AI
+        //base.SubscribeAllEvents(_translator);
+    }
+
+    public TypeSchema Schema => _translator.Validator.Schema;
+
+    protected override async Task ProcessRequestAsync(string input, CancellationToken cancelToken)
+    {
+        Order order = await _translator.TranslateAsync(input);
+
+        string json = Json.Stringify(order);
+        Console.WriteLine(json);
+
+        if (!PrintAnyUnknown(order))
+        {
+            Console.WriteLine("Success!");
+        }
+    }
+
+    bool PrintAnyUnknown(Order order)
+    {
+        if (order.Items != null)
+        {
+            StringBuilder sb = new StringBuilder();
+            order.GetUnknown(sb);
+            if (sb.Length > 0)
+            {
+                Console.WriteLine("I didn't understand the following:");
+                Console.WriteLine(sb.ToString());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            Restaurant app = new Restaurant();
+            // Un-comment to print auto-generated schema at start:
+            Console.WriteLine(app.Schema.Schema.Text);
+
+            await app.RunAsync("🍕> ", args.GetOrNull(0));
+        }
+        catch (Exception ex)
+        {
+            WriteError(ex);
+            return -1;
+        }
+
+        return 0;
+    }
+}
---
+++ examples/Restaurant/RestaurantImpl.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System.Text;
+
+namespace Restaurant;
+
+/**
+ * We use partial classes to deliberately keep SCHEMA separate from implementation
+ * CoffeeShopSchema.cs defines the shape of the data
+ * CoffeeShopImpl.cs has the code. 
+ **/
+
+public partial class Order
+{
+    public virtual void GetUnknown(StringBuilder sb)
+    {
+        if (Items != null)
+        {
+            foreach (OrderItem item in Items)
+            {
+                item.GetUnknown(sb);
+            }
+        }
+    }
+}
+
+public abstract partial class OrderItem
+{
+    public virtual void GetUnknown(StringBuilder sb) { return; }
+}
+
+public partial class UnknownItem : OrderItem
+{
+    public override void GetUnknown(StringBuilder sb)
+    {
+        sb.AppendLine(Text);
+    }
+}
