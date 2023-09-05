40c2b9008bf444bf14f7d4f5cdfb596d6112f3f3
Abby Harrison <abby.harrison@microsoft.com>
2023-09-05 15:38:04 -0700

    created base project for sqlite demo

---
+++ examples/Bookstore/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.TypeChat;
+using Microsoft.TypeChat.Schema;
+
+namespace Bookstore;
+
+public class BookstoreApp : ConsoleApp
+{
+    public BookstoreApp()
+    {}
+}
