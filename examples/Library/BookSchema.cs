41fee9a0a155c2c769d3a5106f5d88ca6b526cba
Abby Harrison <abby.harrison@microsoft.com>
2023-09-08 12:44:40 -0700

    resolve conflicts

---
+++ examples/Library/BookSchema.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace Library;
+
+[JsonPolymorphic]
+[JsonDerivedType(typeof(LibraryBook), typeDiscriminator: nameof(LibraryBook))]
+[JsonDerivedType(typeof(UnknownBook), typeDiscriminator: nameof(UnknownBook))]
+public class Book
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    [JsonPropertyName("author")]
+    public string Author { get; set; }
+    [JsonPropertyName("genre")]
+    public Genre Genre { get; set; }
+}
+
+public class LibraryBook : Book
+{
+    [JsonPropertyName("isbn")]
+    public string ISBN { get; set; }
+}
+
+
+public class UnknownBook : Book
+{
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Genre
+{
+    Fiction,
+    NonFiction,
+    Fantasy,
+    SciFi,
+    Mystery,
+    Romance,
+    Thriller,
+    Horror,
+    Western,
+    Biography,
+    History,
+    Poetry,
+    Children,
+    YoungAdult,
+    Other
+}
---
+++ examples/Library/Program.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Microsoft.TypeChat;
+using Microsoft.TypeChat.Schema;
+
+namespace Library;
+
+
+public class LibraryApp : ConsoleApp
+{
+    JsonTranslator<Book> _translator;
+
+    public LibraryApp()
+    {
+        _translator = new JsonTranslator<Book>(
+            new LanguageModel(Config.LoadOpenAI()),
+            new TypeValidator<Book>()
+        );
+
+        // Uncomment to see ALL raw messages to and from the AI
+        //base.SubscribeAllEvents(_translator);
+    }
+    public TypeSchema Schema => _translator.Validator.Schema;
+
+    public override async Task ProcessRequestAsync(string input, CancellationToken cancelToken)
+    {
+        Book book = await _translator.TranslateAsync(input);
+
+        string json = Json.Stringify(book);
+        Console.WriteLine(json);
+
+        if (!book.Title.Any() || book is UnknownBook)
+        {
+            Console.WriteLine("Unable to provide a good book :(");
+        }
+        else
+        {
+            Console.WriteLine("Success!");
+        }
+    }
+
+    public static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            LibraryApp app = new LibraryApp();
+            // Un-comment to print auto-generated schema at start:
+            //Console.WriteLine(app.Schema.Schema.Text);
+
+            await app.RunAsync("☕> ", args.GetOrNull(0));
+        }
+        catch (Exception ex)
+        {
+            WriteError(ex);
+            return -1;
+        }
+
+        return 0;
+    }
+}
