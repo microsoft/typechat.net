05b368cdddec48dd5efff32ae46e412a60e43b90
Umesh Madan <umeshma@microsoft.com>
2023-09-18 14:23:50 -0700

    Configure Embeddings using Env variable

--- tests/TypeChat.Tests/TestConfig.cs
+++ tests/TypeChat.Tests/TestConfig.cs
@@ -10,23 +10,57 @@ public class TestConfig : TypeChatTest
         string? prevEndpoint = null;
         string? prevKey = null;
         string? prevOrg = null;
+        string? prevModel = null;
         try
         {
             prevEndpoint = SetEnv(OpenAIConfig.VariableNames.OPENAI_ENDPOINT, "O_ENDPOINT");
             prevKey = SetEnv(OpenAIConfig.VariableNames.OPENAI_API_KEY, "O_API");
             prevOrg = SetEnv(OpenAIConfig.VariableNames.OPENAI_ORGANIZATION, "O_ORG");
+            prevModel = SetEnv(OpenAIConfig.VariableNames.OPENAI_MODEL, "O_MODEL");
 
             var config = OpenAIConfig.FromEnvironment();
             Assert.False(config.Azure);
             Assert.Equal("O_ENDPOINT", config.Endpoint);
             Assert.Equal("O_API", config.ApiKey);
             Assert.Equal("O_ORG", config.Organization);
+            Assert.Equal("O_MODEL", config.Model);
         }
         finally
         {
             SetEnv(OpenAIConfig.VariableNames.OPENAI_ENDPOINT, prevEndpoint);
             SetEnv(OpenAIConfig.VariableNames.OPENAI_API_KEY, prevKey);
             SetEnv(OpenAIConfig.VariableNames.OPENAI_ORGANIZATION, prevOrg);
+            SetEnv(OpenAIConfig.VariableNames.OPENAI_MODEL, prevModel);
+        }
+    }
+
+    [Fact]
+    public void TestEnvOpenAIEmbedding()
+    {
+        string? prevEndpoint = null;
+        string? prevKey = null;
+        string? prevOrg = null;
+        string? prevModel = null;
+        try
+        {
+            prevEndpoint = SetEnv(OpenAIConfig.VariableNames.OPENAI_ENDPOINT, "O_ENDPOINT");
+            prevKey = SetEnv(OpenAIConfig.VariableNames.OPENAI_API_KEY, "O_API");
+            prevOrg = SetEnv(OpenAIConfig.VariableNames.OPENAI_ORGANIZATION, "O_ORG");
+            prevModel = SetEnv(OpenAIConfig.VariableNames.OPENAI_EMBEDDINGMODEL, "O_EMBEDDING");
+
+            var config = OpenAIConfig.FromEnvironment(true);
+            Assert.False(config.Azure);
+            Assert.Equal("O_ENDPOINT", config.Endpoint);
+            Assert.Equal("O_API", config.ApiKey);
+            Assert.Equal("O_ORG", config.Organization);
+            Assert.Equal("O_EMBEDDING", config.Model);
+        }
+        finally
+        {
+            SetEnv(OpenAIConfig.VariableNames.OPENAI_ENDPOINT, prevEndpoint);
+            SetEnv(OpenAIConfig.VariableNames.OPENAI_API_KEY, prevKey);
+            SetEnv(OpenAIConfig.VariableNames.OPENAI_ORGANIZATION, prevOrg);
+            SetEnv(OpenAIConfig.VariableNames.OPENAI_EMBEDDINGMODEL, prevModel);
         }
     }
 
