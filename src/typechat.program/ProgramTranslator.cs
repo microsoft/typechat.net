ee10242b95d88383c5c32c39b5b1e9d9e65cad84
Umesh Madan <umeshma@microsoft.com>
2023-09-22 10:55:35 -0700

    ProgramSchema  (#151)
    Moved ProgramSchema into a const string

--- src/typechat.program/ProgramTranslator.cs
+++ src/typechat.program/ProgramTranslator.cs
@@ -11,6 +11,38 @@ namespace Microsoft.TypeChat;
 /// </summary>
 public class ProgramTranslator : JsonTranslator<Program>
 {
+    internal const string SchemaText =
+@"
+// A program consists of a sequence of function calls that are evaluated in order.
+export type Program = {
+    ""@steps"": FunctionCall[];
+    // Collect parts of the user request that could not translated.
+    // Including if NO suitable API provided
+    ""@cannot_translate""?: string[];
+}
+
+// A function call specifies a function name and a list of argument expressions. Arguments may contain
+// nested function calls and result references.
+export type FunctionCall = {
+    // Name of the function
+    ""@func"": string;
+    // Arguments for the function, if any
+    ""@args""?: Expression[];
+};
+
+// An expression is a JSON value, a function call, or a reference to the result of a preceding expression.
+export type Expression = JsonValue | FunctionCall | ResultReference;
+
+// A JSON value is a string, a number, a boolean, null, an object, or an array. Function calls and result
+// references can be nested in objects and arrays.
+export type JsonValue = string | number | boolean | null | { [x: string]: Expression } | Expression[];
+
+// A result reference represents the value of an expression from a preceding step.
+export type ResultReference = {
+    // Index of the previous expression in the ""@steps"" array
+    ""@ref"": number;
+};
+";
     internal static readonly TypescriptSchema ProgramSchema;
 
     static ProgramTranslator()
@@ -25,7 +57,7 @@ public class ProgramTranslator : JsonTranslator<Program>
     /// <returns></returns>
     internal static TypescriptSchema GetProgramSchema()
     {
-        return TypescriptSchema.Load(typeof(Program), "ProgramSchema.ts");
+        return new TypescriptSchema(typeof(Program), SchemaText);
     }
 
     SchemaText _apiDef;
