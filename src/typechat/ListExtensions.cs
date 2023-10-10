fc27d13f4c98429faf6b08e303b2d85382f6889d
Shawn Callegari <36091529+shawncal@users.noreply.github.com>
2023-10-10 15:46:33 -0700

    Extensions Refactor

---
+++ src/typechat/ListExtensions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.TypeChat;
+
+internal static class ListExtensions
+{
+    public static bool IsNullOrEmpty<T>(this IList<T> list)
+    {
+        return (list == null || list.Count == 0);
+    }
+}
