0e6b54a1842b0cb8e8c243f48589b0b2bd5ecebe
Umesh Madan <umeshma@microsoft.com>
2023-11-01 15:02:25 -0700

    Fix Issue 191 (#193)
    This PR fixes an issue with the how the PromptLibrary in TypeChat examples adds the current date time.

--- examples/typechat.examplesLib/PromptLibrary.cs
+++ examples/typechat.examplesLib/PromptLibrary.cs
@@ -14,7 +14,7 @@ public class PromptLibrary
     public static PromptSection Now()
     {
         DateTime now = DateTime.Now;
-        PromptSection section = $"Use precise date and times RELATIVE TO CURRENT DATE: {now.ToLongDateString()} CURRENT TIME: {now.ToLongTimeString()}";
+        PromptSection section = $"Use precise date and times RELATIVE TO CURRENT DATE: {now.ToLongDateString()} CURRENT TIME: {now.ToLongTimeString()}\n";
         section += "Also turn ranges like next week and next month into precise dates";
         return section;
     }
