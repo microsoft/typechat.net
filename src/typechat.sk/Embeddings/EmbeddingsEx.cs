1585a006ec4fce75c28dc79361581e760cf3986a
Robert Gruen <robgruen@microsoft.com>
2025-10-16 12:49:13 -0700

    Resolved all obsolete warnings.

--- src/typechat.sk/Embeddings/EmbeddingsEx.cs
+++ src/typechat.sk/Embeddings/EmbeddingsEx.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using Microsoft.Extensions.AI;
+
 namespace Microsoft.TypeChat.Embeddings;
 
 public static class EmbeddingsEx
@@ -11,12 +13,28 @@ public static class EmbeddingsEx
     /// <param name="text">text for which to generate an embedding</param>
     /// <param name="kernel">kernel to use</param>
     /// <param name="cancelToken">optional cancel token</param>
-    /// <returns></returns>
+    /// <returns>The embedding of the text.</returns>
+    [Obsolete("Use Task<Embedding<float>> GenerateEmbeddingAsync(this IEmbeddingGenerator<string, Embedding<float>> model, string text, Kernel kernel, CancellationToken cancelToken = default) instead. ")]
     public static async Task<Embedding> GenerateEmbeddingAsync(this ITextEmbeddingGenerationService model, string text, Kernel kernel, CancellationToken cancelToken = default)
     {
         string[] texts = new string[] { text };
         var results = await model.GenerateEmbeddingsAsync(texts, kernel, cancelToken).ConfigureAwait(false);
         return new Embedding(results[0]);
+    }
+
+    /// <summary>
+    /// Generate an embedding using the given model
+    /// </summary>
+    /// <param name="model">model to use</param>
+    /// <param name="text">text for which to generate an embedding</param>
+    /// <param name="kernel">kernel to use</param>
+    /// <param name="cancelToken">optional cancel token</param>
+    /// <returns>The embedding of the text.</returns>
+    public static async Task<Embedding<float>> GenerateEmbeddingAsync(this IEmbeddingGenerator<string, Embedding<float>> model, string text, Kernel kernel, CancellationToken cancelToken = default)
+    {
+        string[] texts = new string[] { text };
+        var results = await model.GenerateAsync(texts, null, cancelToken);
+        return results[0];
     }
 
     /// <summary>
